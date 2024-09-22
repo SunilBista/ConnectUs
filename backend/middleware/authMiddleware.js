a571d6e138bff7951b55b6b8473a9fc4d40d369d
SunilBista <sunilbista303@gmail.com>
2024-09-22 13:33:54 +1000

    feat: add middleware for checking token

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const secretKey = process.env.JWT_SECRET;
+
+const checkAuth = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error_message: "No token, authorization denied" });
+  }
+  try {
+    const decodedInfo = jwt.verify(token, secretKey);
+    console.log("decodedInfo", decodedInfo);
+    req.user = decodedInfo.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ error_message: "Token is not valid" });
+  }
+};
+
+module.exports = checkAuth;
