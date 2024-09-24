b0ce66d3fcd8915d4cb1dc5664c8104d42626548
SunilBista <sunilbista303@gmail.com>
2024-09-24 14:42:54 +1000

    feat: add auth service for login, sign up and user

---
+++ frontend/frontend/src/services/authService.jsx
@@ -0,0 +1,62 @@
+import makeAPICall from "../api/apiClient";
+
+const login = async (email, password) => {
+  try {
+    const res = await makeAPICall({
+      method: "POST",
+      endpoint: "/api/auth/login",
+      payload: {
+        email,
+        password,
+      },
+      contentType: "application/json",
+    });
+    console.log(res);
+    // if (res.data.token) {
+    //   localStorage.setItem("token", res.data.token);
+    // }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const signup = async (email, password, timezone) => {
+  try {
+    const res = await makeAPICall({
+      methof: "POST",
+      endpoint: "/api/auth/signup",
+      payload: {
+        email,
+        password,
+        timezone,
+      },
+      contentType: "application/json",
+    });
+    // if (res.data.token) {
+    //   localStorage.setItem("token", res.data.token);
+    // }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getUser = async () => {
+  try {
+    const res = await makeAPICall({
+      method: "GET",
+      endpoint: "/api/auth/user",
+    });
+    console.log(res);
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export { login, signup, logout, getUser };
