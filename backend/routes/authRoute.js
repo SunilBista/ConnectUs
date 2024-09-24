dd20ae08bee42f4f6c40b6933cfe0af8f30a55ea
SunilBista <sunilbista303@gmail.com>
2024-09-24 14:39:41 +1000

    feat: added user info route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { userLogin, userSignup } = require("../controllers/authController");
+const {
+  userLogin,
+  userSignup,
+  getUser,
+} = require("../controllers/authController");
+const checkAuth = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/login", userLogin);
 router.post("/signup", userSignup);
+router.get("/user", checkAuth, getUser);
 
 module.exports = router;
