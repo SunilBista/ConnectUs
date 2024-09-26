e534e7c687966d14d6afe06e252dbb8d5d72906f
SunilBista <sunilbista303@gmail.com>
2024-09-26 22:57:49 +1000

    misc: update tailwind config

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -1,8 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/Authentication/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg-color": "#EDE8DC",
+        "secondary-bg-color": "#F8F6F1",
+
+        "primary-text-color": "#4A4A4A",
+        "secondary-text-color": "#7D7D7D",
+        "muted-text-color": "#A89C8A",
+        "error-text-color": "#8B3B3B",
+        "success-text-color": "#4A4A4A",
+
+        "primary-btn-color": "#C2D9C9",
+        "primary-btn-hover-color": "#A8C1B1",
+        "accent-btn-color": "#B3CADB",
+        "accent-btn-hover-color": "#97B3C3",
+        "disabled-btn-color": "#D1C6B2",
+
+        "input-bg-color": "#FFFFFF",
+        "input-border-color": "#D1C6B2",
+        "input-placeholder-color": "#A89C8A",
+        "focus-border-color": "#A89C8A",
+
+        "primary-border-color": "#D1C6B2",
+        "hover-border-color": "#A89C8A",
+
+        "error-bg-color": "#F5B7B1",
+        "success-bg-color": "#C2D9C9",
+
+        "shadow-color": "rgba(0, 0, 0, 0.1)",
+      },
+    },
+    variants: {
+      extend: {
+        borderColor: ["focus"],
+      },
+    },
   },
   plugins: [],
 };
