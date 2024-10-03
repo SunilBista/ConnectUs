a218fb18abb491dd8f8feb7306cbfcb49ca36a61
SunilBista <sunilbista303@gmail.com>
2024-10-03 20:49:40 +1000

    feat: add settings page

---
+++ frontend/frontend/src/components/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="flex h-full justify-center p-4 items-center flex-col bg-secondary-bg-color md:p-6 lg:p-8">
+      <h1 className="text-primary-text-color">Welcome to the settings</h1>
+    </div>
+  );
+};
+
+export default Settings;
