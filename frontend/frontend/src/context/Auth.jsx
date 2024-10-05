12eca4bf7c3f810844eb9711c1826992d17e4393
SunilBista <sunilbista303@gmail.com>
2024-10-05 20:59:12 +1000

    feat: handle data and implement logout

--- frontend/frontend/src/context/Auth.jsx
+++ frontend/frontend/src/context/Auth.jsx
@@ -1,20 +1,21 @@
 import React, { createContext, useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
 import cookies from "browser-cookies";
-import { getUser } from "../services/authService";
+import { getUser, logout } from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const token = cookies.get("token");
     async function fetchData() {
       try {
         const userResponse = await getUser();
-        setUser(userResponse?.data?._id);
+        console.log("user", userResponse.data);
+        setUser(userResponse?.data);
       } catch (error) {
         console.error("Invalid token", error);
         setUser(null);
@@ -26,8 +27,20 @@ const AuthProvider = ({ children }) => {
     setLoading(false);
   }, []);
 
+  const handleLogout = async () => {
+    try {
+      await logout();
+      setUser(null);
+      navigate("/login");
+    } catch (err) {
+      const { data = {} } = err?.response;
+      const { message } = data;
+      console.error("Logout failed", data, message);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, loading, setUser }}>
+    <AuthContext.Provider value={{ user, loading, setUser, handleLogout }}>
       {children}
     </AuthContext.Provider>
   );
